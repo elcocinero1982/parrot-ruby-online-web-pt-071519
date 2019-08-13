672dc02d2545bf13dc998bc57537209a636d5c90
jose luis <cortezluisjose@gmail.com>
2019-08-13 21:07:49 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase andhttps://learn.co/tracks/online-software-engineering-structured/procedural-ruby/variables-and-methods/return-values-lab#
 # returns the phrase
-def parrot
+def parrot(phrase)
+  parrot = "phrase"
 parrot ="Squawk!"
 puts "Squawk!"
 return "Squawk!"
